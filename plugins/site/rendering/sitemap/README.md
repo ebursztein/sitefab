b7e09e2ad37b7f43ab702d3ba1861108fca5391b
Celine Bursztein <bitbucket@celine.im>
2017-01-15 15:05:42 -0800

    Sitemap plugin

---
+++ plugins/site/rendering/sitemap/README.md
@@ -0,0 +1,18 @@
+# Sitemap
+
+Create the RSS.xml file for the entire website.
+
+## Usage
+
+Specify which directory to copy and its destination in the configuration file
+
+### Configuration
+For example in config.yaml
+```yaml
+template: "rss.xml"
+```
+
+will use sitemap.xml in /templates to generate sitemap
+
+## Credit
+Elie & Celine Bursztein
--- plugins/site/rendering/sitemap/sitemap.py
+++ plugins/site/rendering/sitemap/sitemap.py
@@ -2,17 +2,39 @@ from SiteFab.Plugins import SiteRendering
 from SiteFab.SiteFab import SiteFab
 from SiteFab import files
 
+
 class Sitemap(SiteRendering):
-    def process(self, unused, site):
-        template_name = site.config.plugins.sitemap.template
+    def process(self, unused, site, config):
+        template_name = config.template
         
         # Loading
         try:
             template = site.jinja2.get_template(template_name)
         except:
-            return (SiteFab.ERROR, "sitemap", "can't find template:%s" % template_name)
+            return SiteFab.ERROR, "sitemap", "can't find template:%s" % template_name
         
         # Rendering
+        for post in site.posts:
+            # add priority and frequency
+            post.meta.priority = 0.7
+            post.meta.frequency = "monthly"
+            if post.meta.microdata_type == "AboutPage":  # about page
+                post.meta.priority = 0.8
+            if post.meta.microdata_type == "Blog":  # Blog page
+                post.meta.priority = 1.0
+                post.meta.frequency = "daily"
+            if post.meta.microdata_type == "WebSite":  # main page
+                post.meta.priority = 1.0
+                post.meta.frequency = "daily"
+            if post.meta.microdata_type == "CollectionPage":  # publications page
+                post.meta.priority = 0.8
+                post.meta.frequency = "daily"
+
+        for col in site.collections:
+            # add priority and frequency
+            col.meta.priority = 0.7
+            col.meta.frequency = "daily"
+
         try:
             rv = template.render(posts=site.posts, collections=site.collections, site_vars=site.vars)
         except Exception as e:
@@ -23,4 +45,4 @@ class Sitemap(SiteRendering):
         files.write_file(path, 'sitemap.xml', rv)
 
         log_info = "template used:%s<br>ouput:%ssitemap.xml" % (template_name, path)
-        return (SiteFab.OK, "sitemap", log_info)
+        return SiteFab.OK, "sitemap", log_info
