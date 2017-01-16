a1c2053688a4cc8c0168467e8370b96017f6f473
Celine Bursztein <bitbucket@celine.im>
2017-01-15 16:57:27 -0800

    Debug compute full url plugin

--- plugins/post/processor/compute_full_post_url/compute_full_post_url.py
+++ plugins/post/processor/compute_full_post_url/compute_full_post_url.py
@@ -5,13 +5,16 @@ from SiteFab.SiteFab import SiteFab
 class FullUrl(PostProcessor):
     def process(self, post, site, config):
         if post.meta.permanent_url:
+
+            permanent_url = post.meta.permanent_url.strip()
             if site.config.url[-1] == "/":
-                post.meta.full_url = "%s%s" % (site.config.url, post.meta.permanent_url)
+                post.meta.full_url = "%s%s" % (site.config.url, permanent_url)
             else:
-                post.meta.full_url = "%s/%s" % (site.config.url, post.meta.permanent_url)
+                post.meta.full_url = "%s/%s" % (site.config.url, permanent_url)
 
             log_info = "full url:%s" % post.meta.full_url
 
             return SiteFab.OK, post.meta.title, ""
         else:
-            return SiteFab.SKIPPED, post.meta.title, ""
+            post.meta.full_url = site.config.url
+            return SiteFab.OK, post.meta.title, ""
