97831d67ba2c3fa7fe7eeda8aab0fc0de61ce455
Celine Bursztein <bitbucket@celine.im>
2017-01-22 15:59:51 -0800

    Download banner in backup plugin

--- plugins/post/processor/backup_media/backup_media.py
+++ plugins/post/processor/backup_media/backup_media.py
@@ -1,8 +1,6 @@
 from tqdm import tqdm
 from multiprocessing import Pool
 import requests
-from requests_toolbelt import SSLAdapter
-from itertools import repeat
 import os
 
 import urlparse
@@ -10,6 +8,7 @@ import urlparse
 from SiteFab.Plugins import PostProcessor
 from SiteFab.SiteFab import SiteFab
 from SiteFab import files
+
 
 def download(info):
     #adapter = SSLAdapter('TLSv1')
@@ -25,19 +24,30 @@ def download(info):
     else:
         return [False, url]
 
+
 class BackupMedia(PostProcessor):
     def process(self, post, site, config):
         if post.md and len(post.md):
-            p = Pool(5) # num thread
+            p = Pool(5)  # num thread
             path = os.path.join(files.get_site_path(), config.backup_dir)
             path_post = post.meta.permanent_url.split('/')[-1]
             to_dl = []
+
+            # # banner
+            if post.meta.banner:
+                banner = urlparse.urlparse(post.meta.banner)
+                extension = banner.path[-4:]
+                banner_name = "%s%s" % (path_post, extension)
+                local_path = os.path.join(path, "banner/")
+                to_dl.append((post.meta.banner, local_path, banner_name))
+
+            # # images
             for img in post.elements.images:
                 a = urlparse.urlparse(img)
                 fname = os.path.basename(a.path)
                 local_path = os.path.join(path, "images/", path_post)
                 to_dl.append((img, local_path, fname))
-
+            # files
             if post.meta.files:
                 for f in post.meta.files.values():
                     a = urlparse.urlparse(f)
@@ -45,9 +55,9 @@ class BackupMedia(PostProcessor):
                     local_path = os.path.join(path, "files/", path_post)
                     to_dl.append((f, local_path, fname))
 
+
             progress_bar = tqdm(total=len(to_dl), unit=' files', desc="Files to dl", leave=False)
 
-            #args = zip(to_dl,post.meta.slug)
             log = ""
             for result in p.imap_unordered(download, to_dl):
             #for i in to_dl:
