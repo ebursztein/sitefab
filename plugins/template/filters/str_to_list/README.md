9b50e4f59cdbbf816e05b9a43ffdb068c5212b4a
Elie <git@elie.net>
2017-06-30 08:08:01 -0700

    Refactored split_to_string filter into str_to_list filter to make it more generic

---
+++ plugins/template/filters/str_to_list/README.md
@@ -0,0 +1,19 @@
+# str_to_list
+
+Transform a string into a list of characters
+
+## Usage
+
+You can use this custom filter in your templates
+
+```jinja2
+{{ meta.title | str_to_list }
+```
+
+## Changelog
+
+- 04/14/17 initial version
+
+## Credits
+
+**Author**: Nicolas VARROT
---
+++ plugins/template/filters/str_to_list/str_to_list.py
@@ -0,0 +1,14 @@
+from SiteFab.Plugins import TemplateFilter
+
+class str_to_list(TemplateFilter):
+    """Transform a string into a list of characters"""
+
+    @staticmethod
+    def myfilter(filter_input):
+        """
+        Args:
+            input (str): input list
+        Return:
+            list: list of characters
+        """
+        return list(filter_input)
