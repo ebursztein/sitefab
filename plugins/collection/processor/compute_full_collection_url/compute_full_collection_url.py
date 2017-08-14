c5e1dcc718c602a337bb08a1530083beb5c84dea
Celine Bursztein <contact@celine.im>
2017-08-13 23:47:00 -0700

    fix error full_url collection

--- plugins/collection/processor/compute_full_collection_url/compute_full_collection_url.py
+++ plugins/collection/processor/compute_full_collection_url/compute_full_collection_url.py
@@ -6,6 +6,6 @@ class FullUrl(CollectionProcessor):
 
     def process(self, collection, site, config):
         safe = collection.meta.name.replace(" ", "-").lower()
-        url = "%s%s%s" % (site.config.url, site.config.collections.output_dir, safe)
+        url = "%s/%s%s" % (site.config.url, site.config.collections.output_dir, safe)
         collection.meta.full_url = url
         return SiteFab.OK, collection.meta.name, url
