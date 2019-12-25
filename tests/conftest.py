94359508301741601c485de1e1ee3cb7da6bbb14
Elie <git@elie.net>
2019-12-25 06:28:25 -0800

    Moved from tests from fake data to sitefab-template

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,19 @@
 from pathlib import Path
+
 import pytest
-from sitefab.SiteFab import SiteFab
+import git
+from termcolor import cprint
+
 from sitefab import __version__ as version
 from sitefab import utils
+from sitefab.SiteFab import SiteFab
+
 # NOTE you don't need to import fixture explictly pytest do it for you. Avoid
 # flake8 errors
 
-
 TEST_ROOT_DIR = Path(__file__).parent
+TEMPLATE_DATA_PATH = TEST_ROOT_DIR / 'sitefab_template'
+TEMPLATE_GIT_URL = 'https://github.com/ebursztein/sitefab-template'
 
 
 @pytest.fixture()
@@ -19,7 +25,7 @@ def myobjdict():
 @pytest.fixture(scope="session")
 def sitefab():
     "load a valid instance of sitefab"
-    fname = TEST_ROOT_DIR / "data" / "config" / "valid_config.yaml"
+    fname = TEMPLATE_DATA_PATH / "config" / "sitefab.yaml"
     print('SiteFab version: %s' % version)
     return SiteFab(fname)
 
@@ -35,3 +41,14 @@ def empty_post():
     post.meta.toc = utils.create_objdict()
     post.elements = utils.create_objdict()
     return post
+
+
+def pytest_configure(config):
+
+    if TEMPLATE_DATA_PATH.exists():
+        cprint('Pulling latest sitefab template', 'green')
+        g = git.cmd.Git(TEMPLATE_DATA_PATH)
+        g.pull()
+    else:
+        cprint('Cloning sitefab template', 'yellow')
+        git.Repo().clone_from(TEMPLATE_GIT_URL, TEMPLATE_DATA_PATH)
