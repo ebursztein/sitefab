b8cfd61e43a69a1b5f3cb860a9223eae53fc35ba
Elie <git@elie.net>
2019-12-25 11:35:33 -0800

    useful code for parse

---
+++ tests/parser/test_parse.py
@@ -0,0 +1,9 @@
+from sitefab.SiteFab import SiteFab
+from sitefab.files import read_file
+from sitefab.parser import Parser
+# FIXME: implement parsing a simple post to make sure everything works as expected
+# sf = SiteFab('../sitefab-template/config/sitefab.yaml', 'whatever')
+# parser = Parser(sf.config.parser, sf)
+
+# md = read_file('../sitefab-template/content/posts/18-4-of-us-internet-users-got-at-least-one-of-their-account-compromised.md')
+# post = parser.parse(md)
