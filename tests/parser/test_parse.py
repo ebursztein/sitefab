14002354bb52d0362d4ef22125f190bd976ff480
Elie <git@elie.net>
2019-12-28 11:10:29 -0800

    disable test parse for now

--- tests/parser/test_parse.py
+++ tests/parser/test_parse.py
@@ -1,9 +1,10 @@
-from sitefab.SiteFab import SiteFab
-from sitefab.files import read_file
-from sitefab.parser import Parser
-# FIXME: implement parsing a simple post to make sure everything works as expected
+# from sitefab.SiteFab import SiteFab
+# from sitefab.files import read_file
+# from sitefab.parser import Parser
+# FIXME: implement parsing a simple post to make sure everything works\
+#  as expected
 # sf = SiteFab('../sitefab-template/config/sitefab.yaml', 'whatever')
 # parser = Parser(sf.config.parser, sf)
 
-# md = read_file('../sitefab-template/content/posts/18-4-of-us-internet-users-got-at-least-one-of-their-account-compromised.md')
+# md = read_file('../sitefab-template/content/posts/18-4-of-us-internet-users-got-at-least-one-of-their-account-compromised.md')  # noqa
 # post = parser.parse(md)
