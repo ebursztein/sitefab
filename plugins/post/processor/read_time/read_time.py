c2df5d1a1ee0244806edf82997ef0e9dc17afb3c
Elie <git@elie.net>
2019-12-28 16:28:31 -0800

    Read time bug fixes

--- plugins/post/processor/read_time/read_time.py
+++ plugins/post/processor/read_time/read_time.py
@@ -1,5 +1,6 @@
 from sitefab.Plugins import PostProcessor
 from sitefab.SiteFab import SiteFab
+from tabulate import tabulate
 
 
 class ReadTime(PostProcessor):
@@ -15,10 +16,13 @@ class ReadTime(PostProcessor):
 
             # reading time in minutes
             post.meta.read_time = max(1, num_sec // 60)
-            st = "%s min read - %s words - %s images<br>%s wps - %s sec read\
-                - %s num_images" % (post.meta.read_time, post.nlp.counts.words,
-                                    num_images, round(wps, 1), round(num_sec),
-                                    num_images)
-            return (SiteFab.OK, post.meta.title, st)
+            info = [
+                ['word per minute', wps],
+                ['words', post.nlp.stats.counts.words],
+                ['num images', num_images],
+                ['read time', post.meta.read_time]
+            ]
+
+            return (SiteFab.OK, post.meta.title, tabulate(info))
         else:
             return (SiteFab.SKIPPED, post.meta.title, "")
